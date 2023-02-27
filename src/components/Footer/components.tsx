4b2ec07a54322fb0fa604830b8a865e0d9b75bfb
felipendelicia <delicia4581@gmail.com>
2023-02-27 17:50:12 -0300

    home page

--- src/components/Footer/components.tsx
+++ src/components/Footer/components.tsx
@@ -3,11 +3,12 @@ import assetsPath from "../../data/assets-path";
 
 export const FooterContainer = styled.div`
     background-image: url(${assetsPath.footer});
+    background-size: cover;
     position: fixed;
     bottom: 0;
     left: 0;
     width:100%;
-    height: 10%;
+    height: 15%;
     padding: 1em 2em;
     display: grid;
     grid-template-columns: 50% 50%;
