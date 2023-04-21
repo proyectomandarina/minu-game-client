f46614b5fcde5cb634f61662d423fc721f20e6c1
Felipe <delicia4581@gmail.com>
2023-04-21 13:15:14 -0300

    start button hover -> get bigger

--- src/components/StartButton/components.tsx
+++ src/components/StartButton/components.tsx
@@ -6,13 +6,24 @@ export const StartButtonContainer = styled.div`
     flex-direction: column;
     align-items: center;
     gap: 10px;
+
+    &:hover {
+        img {
+            transform: scale(1.15);
+        }
+        p{
+            font-size: 1.75em;
+        }
+    }
 `
 
 export const StartButtonSvg = styled.img`
+    transition: transform 0.2s ease-out;
     width: 10em;
 `
 
 export const StartLegend = styled.p`
+    transition: font-size 0.2s ease-out;
     color: ${theme.white};
     -webkit-text-stroke: 2px ${theme.borderTextColor};
     font-size: 1.5em;
