aa7d2c613fbdc20d9afc9a1c3e2532f410761ec6
Felipe <delicia4581@gmail.com>
2023-04-21 12:26:02 -0300

    stroke width sintaxis

--- src/components/BackArrow/index.tsx
+++ src/components/BackArrow/index.tsx
@@ -43,7 +43,7 @@ export default function BackArrow({ page, color }: { page: STEPS, color: string 
           height="40"
           rx="14"
           stroke={color}
-          stroke-width="2"
+          strokeWidth="2"
         />
       </Icon>
     </Container>
